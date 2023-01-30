5c8f8ba0d7f56f8bc643c2a88a4b9f77cd944e59
ismael <bilalclyde@protonmail.ch>
2023-01-30 14:53:51 +0100

    first Xcode commit

---
+++ Tutorials.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+
+var greeting = "Hello, playground"
+
+greeting = "new string"
+var x, y: Int
+print(greeting)
+
+x = 20
+y = 5
+
+func buyCheeseCoin(amount: Int, price: Double) {
+    print("we just bought \(amount) 🧀🪙")
+}
+
+buyCheeseCoin(amount: 3, price: 1.2345)
